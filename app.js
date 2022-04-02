9282b6616816f6396b12770909e14952e4a563ac
AxelBroeglin <axelbroeglin@gmail.com>
2022-04-02 20:12:47 +0100

    Preparing for JS

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+1/ click on a button selects it, changing its color. Clicking on a new button 
+selects the new one, resetting color of the previous one
+2/ Click on submit checks which grade was chosen, keeps it so it can be 
+displayed on next screen
+3/ Next screen appears by applying display:none; to 1st screen and deleting it
+from screen 2
+*/
