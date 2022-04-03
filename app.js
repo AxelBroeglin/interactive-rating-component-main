4f2ecd70fb586bae1fa182c9f712b4afa84325b2
AxelBroeglin <axelbroeglin@gmail.com>
2022-04-03 21:28:01 +0100

    1st work on js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /*
-1/ click on a button selects it, changing its color. Clicking on a new button 
+1/ click on a grade selects it, changing its color. Clicking on a new grade 
 selects the new one, resetting color of the previous one
 2/ Click on submit checks which grade was chosen, keeps it so it can be 
 displayed on next screen
 3/ Next screen appears by applying display:none; to 1st screen and deleting it
 from screen 2
 */
+
+const btn = document.getElementsByClassName('main-container__button')
+
+//btn.addEventListener('click', function onClick() {
+    
+});
