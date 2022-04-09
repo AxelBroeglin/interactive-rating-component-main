bdcefa3aa9f1bef75972d3b0ff49da51e30d3088
AxelBroeglin <axelbroeglin@gmail.com>
2022-04-09 17:14:49 +0100

    Trying to add class to button

--- app.js
+++ app.js
@@ -10,30 +10,35 @@ from screen 2
 //document.getElementsByClassName creates an array. It needs to be used as such
 
 const gradeContainers = document.querySelectorAll('.gradeContainer');
-const gradeActive = document.querySelectorAll('.active');
+//const gradeActive = document.querySelectorAll('.active');
 
+const submitBtn = document.querySelectorAll('.main-container__button');
+// Button is recognized onclick
+console.log(submitBtn)
+
+//Add class Active to clicked grade
 gradeContainers.forEach((gradeContainer) => {
     gradeContainer.addEventListener('click', () => {
-    removeClasses();
-    addClasses(gradeContainer);
-    removeActive();
+      gradeContainers.forEach((gradeContainer) => {
+        gradeContainer.classList.remove('active');
+      });    
+      if (gradeContainer.classList.contains("active")) {
+        gradeContainer.classList.remove("active");
+      }
+      else {
+       gradeContainer.classList.add("active");
+      }
   });
 });
 
-function removeClasses() {
-    gradeContainers.forEach((gradeContainer) => {
-    gradeContainer.classList.remove("active");
+// Console logs what user clicked and adds class to it. Problem : has document, not precise enough.
+  document.addEventListener('click', function handleClick(event) {
+    console.log('user clicked: ', event.target);
+    
+    event.target.classList.add('bg-yellow');
   });
-}
 
-function addClasses(gradeContainer) {
-    gradeContainer.classList.add("active");
-}
-
-function removeActive() {
-  if (gradeContainer.classList.contains('active')) {
-    gradeContainer.classList.remove("active");
-  }
-}
-
-function
+// Displays Hello world when button is clicked
+  document.getElementById("main-container__button").addEventListener("click", function() {
+    alert("Hello World!");
+  });
